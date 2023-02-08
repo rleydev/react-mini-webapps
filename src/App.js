469b4265244a08919a2404a9c719ad12f4e18147
Arthur Lee <rley.dev@gmail.com>
2023-02-09 02:59:14 +0600

    feat: add project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './compoments/Header';
+import Meme from './compoments/Meme';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
---
+++ src/compoments/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="../../meme-logo.png" alt="logo" className="header--image"/>
+            <h4 className="header--project">Tablet-Mobile Version</h4>
+        </header>
+    )
+}

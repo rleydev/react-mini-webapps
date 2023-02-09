b108db615a7b7eea74e9091f81b116aff8b6dac9
Arthur Lee <rley.dev@gmail.com>
2023-02-10 02:30:31 +0600

    feat: win logic

--- src/App.js
+++ src/App.js
@@ -2,35 +2,58 @@ import React from 'react';
 import './App.css';
 import Die from './Die';
 import {nanoid} from 'nanoid'
+import ReactConfetti from 'react-confetti';
 
 function App() {
 
   const [dice, setDice] = React.useState(allNewDies())
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+      console.log("WIN!")
+    }
+    
+  }, [dice])
+
+  function generateNewDie() {
+    return {
+      value:Math.ceil(Math.random() * 6), 
+      isHeld: false,
+      id: nanoid()
+    }
+  }
 
   function allNewDies() {
     const newDice = []
     for(let i = 0; i < 10; i++) {
-      newDice.push({
-        value:Math.ceil(Math.random() * 6), 
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(generateNewDie())
     }
     return newDice
   }
 
   function rollDice() {
-    setDice(allNewDies())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? die : generateNewDie()
+    }))
   }
   
   const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
 
   function holdDice(id) {
-    console.log(id)
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die
+    }))
   }
 
   return (
     <main>
+      <h1 className='title'>Tenzies</h1>
+      <p className='instructions'>Roll until all dice are the same. Click exh die to freeze it at its current value between rolls</p>
       <div className='dice-container'>
         {diceElements}
       </div> 
